43d0de1ea2cdaaffc42fdef19a4d5bfc9ef3e9b7
igor-yamamoto <igorhc.yamamoto@gmail.com>
2021-10-05 22:29:10 -0300

    Configurando gateway A

--- examples/agric_prec/regional/scripts/kafka_create_topic.sh
+++ examples/agric_prec/regional/scripts/kafka_create_topic.sh
@@ -1,14 +1,11 @@
 docker-compose exec broker-A \
-    kafka-topics --create --zookeeper zookeeper:2181 --replication-factor 2 --partitions 10 --topic devices-temperature  --if-not-exists 
-
-# docker-compose exec broker-A \
-#     kafka-topics --create --topic devices-humidity --partitions 1 --replication-factor 2 --if-not-exists --zookeeper zookeeper:2181
-
-# docker-compose exec broker-A \
-#     kafka-topics --create --topic devices-luminosity --partitions 1 --replication-factor 2 --if-not-exists --zookeeper zookeeper:2181
-
-# docker-compose exec broker-A \
-#     kafka-topics --create --topic devices-ph --partitions 1 --replication-factor 2 --if-not-exists --zookeeper zookeeper:2181
+    kafka-topics --create --zookeeper zookeeper:2181 --replication-factor 2 --partitions 10 --topic devices-temperature  --if-not-exists && \
+docker-compose exec broker-A \
+kafka-topics --create --zookeeper zookeeper:2181 --replication-factor 2 --partitions 10 --topic devices-humidity  --if-not-exists && \
+docker-compose exec broker-A \
+kafka-topics --create --zookeeper zookeeper:2181 --replication-factor 2 --partitions 10 --topic devices-luminosity  --if-not-exists && \
+docker-compose exec broker-A \
+kafka-topics --create --zookeeper zookeeper:2181 --replication-factor 2 --partitions 10 --topic devices-ph  --if-not-exists
 
 # docker-compose exec broker-A \
 #     kafka-topics --list --zookeeper zookeeper:2181
