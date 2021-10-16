ca4ae23b597ea35d8bd569b8a073bfdd76b4f53c
igor-yamamoto <igorhc.yamamoto@gmail.com>
2021-10-16 14:30:34 -0300

    Retirando processo de validação se dispositivo é permitido

--- gateway/builds/python/code/scripts/pipeline.py
+++ gateway/builds/python/code/scripts/pipeline.py
@@ -12,27 +12,12 @@ def validate_json(message):
         print(f"Message '{message}' coudn't be serialized as json object.")
         return '', 400
 
-def allowed_devices(message):
-    try:
-        device_guid = message['device_guid']
-        if device_guid in allowed_devices_guid:
-            return message, 0
-        else:
-            print(f"Device with guid '{device_guid}' not set as allowed.")
-            return '', 400
-    except:
-        print(f"Field 'device_guid' not configured. Ignoring")
-        return '', 400
-
 def main(message):
     message, status = validate_json(message)
 
     if status == 0:
-        message, status = allowed_devices(message)
+        message = json.dumps(message)
         context = ''
 
-        if status == 0:
-            message = json.dumps(message)
-
-    # message = json.dumps(message)
-            return message, context
+# message = json.dumps(message)
+        return message, context
